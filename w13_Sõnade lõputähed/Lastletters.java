6d2f4de59201d53230f5ee7844958c84f90d5d4d
Ekkku <egle.vestrik@vikk.ee>
2021-05-07 15:23:43 +0300

    15.23

--- w13_Sõnade lõputähed/Lastletters.java
+++ w13_Sõnade lõputähed/Lastletters.java
@@ -17,33 +17,54 @@ public class Lastletters {
     }
 
     public static void readfile(){
+        ArrayList<String> filewords = new ArrayList<String>();
+        ArrayList<String> words = new ArrayList<String>();
+        ArrayList<String> letters = new ArrayList<String>();
         try {
             File myFile = new File("Toretext.txt");
-            Scanner s = new Scanner(myFile);
-            ArrayList<String> filewords = new ArrayList<String>();
+            Scanner s = new Scanner(myFile);       
             //panen failid listi
             while (s.hasNext()){
                 filewords.add(s.next());
             }
-            //eemaldan 
-            for (int i = 0; i < filewords.size(); i++) {
-                if (filewords.get(i)=="–"){
-                    filewords.remove(i);
-                }
-            }
-            System.out.println(filewords);
-
-
             s.close();
+            
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-
+        //eemaldada üleliigsed märgid    
+        for (int i = 0; i < filewords.size(); i++) {
+            if ((filewords.get(i)).contains(".") ){
+                words.add(((filewords.get(i)).replace(".","")).toLowerCase());                
+            } else if((filewords.get(i)).contains(",")){
+                words.add(((filewords.get(i)).replace(",","")).toLowerCase());
+            } else if((filewords.get(i)).contains("-")||(filewords.get(i)).contains("-")){
+                
+            } else if((filewords.get(i)).contains("!")){
+                words.add(((filewords.get(i)).replace("!","")).toLowerCase());
+            } else if((filewords.get(i)).contains(")")){
+                words.add(((filewords.get(i)).replace(")","")).toLowerCase());
+            } else if((filewords.get(i)).contains("(")){
+                words.add(((filewords.get(i)).replace("(","")).toLowerCase());
+            }else{
+                words.add((filewords.get(i)).toLowerCase());
+            }
+            
+        }
+        System.out.println(words);
+        System.out.println("------");
+        //System.out.println(filewords);
+        for (int i = 0; i < words.size(); i++) {
+            //kui pikk on sõna
+            int lCount = (words.get(i)).length();
+            //leia viimane täht           
+            String lastL = (words.get(i)).substring(lCount-1,lCount);
+            letters.add(lastL);
+        }
+        System.out.println(letters);
         
     }
 
-    public static void lastLetter(String word){
 
-    }
 }
---
+++ w13_Sõnade lõputähed/testing.java
@@ -0,0 +1,9 @@
+public class testing {
+    public static void main(String[] args) {
+        String s = "testimise testimiseks, testime testi";
+        if (s.contains(",")){
+            String a = s.replace(",", "");
+            System.out.println(a);
+        }
+    }
+}
